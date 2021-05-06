c3c95fe8a011941fea366ac3e34eef9f700496e1
Rhenan Bartels <rhenan.bartels@gmail.com>
2021-05-06 09:42:07 -0300

    Init route to execute queries

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.get("/{slug}")
+async def execute(slug: str):
+    return {"slug": slug}
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,11 @@ def test_root_route(test_client):
 
     assert response.status_code == 200
     assert response.json()["message"] == "hello world"
+
+
+def test_execute_query_from_query_slug_request(test_client):
+    query_slug = "query-slug-1"
+    response = test_client.get(f"/{query_slug}")
+
+    assert response.status_code == 200
+    assert response.json()["slug"] == query_slug
