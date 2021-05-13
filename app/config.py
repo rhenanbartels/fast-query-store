b3c4cba3c2fe2c9c7c661634c6f4bc0a4f3b5057
Rhenan Bartels <rhenan.bartels@gmail.com>
2021-05-13 10:48:10 -0300

    Add cache to query execution endpoint

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     queries_file_path: str = os.getenv("queries_file_path", "")
+    cache_ttl: int = os.getenv("cache_ttl", 300)
--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from functools import lru_cache
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 
@@ -18,13 +22,18 @@ def get_settings():
     return Settings()
 
 
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
+
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
 
-# TODO: create db connections pool. (Maybe use a dict)
 @app.get("/query/{slug}")
+@cache(expire=get_settings().cache_ttl)
 async def execute(slug: str, settings: Settings = Depends(get_settings)):
     backend = backends.JsonBackend(file_path=settings.queries_file_path)
     try:
@@ -34,7 +43,7 @@ async def execute(slug: str, settings: Settings = Depends(get_settings)):
 
     db_engine = create_engine(query["db_url"])
     with db_engine.connect() as conn:
-        result_set = conn.execute(query["query"]).fetchall()
+        result_set = jsonable_encoder(conn.execute(query["query"]).fetchall())
 
     db_engine.dispose()
 
