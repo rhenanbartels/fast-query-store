1286a266704e65b71076a19baf0f08f526289952
Rhenan Bartels <rhenan.bartels@gmail.com>
2021-05-18 11:08:06 -0300

    Start cli

---
+++ cli.py
@@ -0,0 +1,13 @@
+import click
+import uvicorn
+
+
+@click.command("run", short_help="Run a development server.")
+@click.option("--host", "-h", default="127.0.0.1", help="The interface to bind to.")
+@click.option("--port", "-p", default=5000, help="The port to bind to.")
+def run_command(host, port):
+    uvicorn.run("app.main:app", host=host, port=port)
+
+
+if __name__ == "__main__":
+    run_command()
